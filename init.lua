3eacde929036a6bc2267a908b2ce2165a963ef7c
nagolove <dekar.own@gmail.com>
2022-01-13 15:49:08 +0300

    genererated source

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local table = _tl_compat and _tl_compat.table or table
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local ipairs = _tl_compat and _tl_compat.ipairs or ipairs; local string = _tl_compat and _tl_compat.string or string; local table = _tl_compat and _tl_compat.table or table
 
+
+require('pipeline')
 
 local colorize = require('ansicolors2').ansicolors
 local inspect = require("inspect")
 local dprint = require('debug_print')
 local debug_print = dprint.debug_print
+local format = string.format
 
 dprint.set_filter({
    [1] = { "joy" },
@@ -24,22 +27,23 @@ debug_print('thread', colorize('%{yellow}>>>>>%{reset} chipmunk_mt started'))
 require("love")
 require("love_inc").require()
 
-
 love.filesystem.setRequirePath("?.lua;?/init.lua;scenes/chipmunk_mt/?.lua")
 
+local pipeline = Pipeline.new("scenes/debug_print_mt")
 local event_channel = love.thread.getChannel("event_channel")
 local main_channel = love.thread.getChannel("main_channel")
 
+local is_stop = false
 local last_render = 0
 
 local threads = {}
 
-local thread_num = 1
+local threads_num = 1
 
-local function init()
-
+local function initThreads()
    local fname = "scenes/debug_print_mt/thread1.lua"
-   for i = 1, thread_num do
+   print(colorize("%{red}" .. format("Using %d threads", threads_num)))
+   for i = 1, threads_num do
       local thread = love.thread.newThread(fname)
 
       table.insert(threads, thread)
@@ -49,27 +53,43 @@ local function init()
    print('threads: ', inspect(threads))
 end
 
-local function render()
+local function initPipeline()
+   pipeline:pushCode("print_debug_filters", [[
+        local dprint = require "debug_print"
+        local col = {1, 1, 1, 1}
+        while true do
+            love.graphics.setColor(col)
 
+            --love.graphics.polygon('fill', verts)
+            --dprint.render(0, 0)
 
+            coroutine.yield()
+        end
+    ]])
 
 
 end
 
-local is_stop = false
+local function printProgramDescription()
+   print(colorize('%{blue}' .. [[description:
+        Программа показывает возможность многопоточной фильтрации логов.
+        Тестирование модуля debug_print()
+        Для переключения фильтров журналирования нажмите цифровые клавиши 1..9 или 0 ]]))
+end
 
-require('pipeline')
-local pipeline = Pipeline.new("scenes/chipmunk_mt")
-pipeline:pushCode("debug_print", [[
-    local dprint = require "debug_print"
-    local col = {1, 1, 1, 1}
-    while true do
-        love.graphics.setColor(col)
-        --love.graphics.polygon('fill', verts)
-        dprint.render(0, 0)
-        coroutine.yield()
-    end
-]])
+local function init()
+   initThreads()
+   initPipeline()
+   printProgramDescription()
+end
+
+local function render()
+   if pipeline:ready() then
+
+      pipeline:openAndClose('print_debug_filters')
+
+   end
+end
 
 local function mainloop()
    while not is_stop do
